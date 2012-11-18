34c1d6dd48725497537461e3357ddf4ac01b9f0d
Viz <antimonyxy@msn.com>
2012-11-18 19:20:30 +0530

    Added console program structs and functions to initialize variables to values. Added readme.txt and settings.txt.

--- GP4_App/main.c
+++ GP4_App/main.c
@@ -1,8 +1,25 @@
+
+/**
+* @author Vijay Kanta
+* @website www.vizcreations.com
+* @abstract Simple console configurator for game Grand Prix 4
+* @license Free software - No guarantee
+* @copyright Not applicable
+* @link https://github.com/vijaykanta/gp4_gfx_config.git
+*/
+
 #include <windows.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include "resource.h"
+
+#define MAX_PATH 1000
+#define MAX_STRUCT 5000
+//#define MAX_LEN 200
+#define MAX_LINE 255
+#define MAX_LINES 100
+#define MAX_VAL 20
 
 #define STRLEN 100
 #define MAX_LEN 5000
@@ -21,7 +38,410 @@ char **lines;
 int val;
 int i, j, k;
 
-void strip_newtab(char *str, int len) {
+char *token;
+char *buff;
+char *value;
+char *comment;
+int val;
+int i;
+char sel;
+//char *token;
+
+char delimiters[] = " .,;|";
+
+typedef struct Configurator {
+	char file_name[256];
+	char *cur_line;
+	char **all_lines;
+	int tot_lines;
+	int cur_sel;
+	FILE *fp;
+	int global;
+	int global_key;
+	char *global_cmt;
+	int res_width;
+	int res_width_key;
+	char *res_width_cmt;
+	int res_height;
+	int res_height_key;
+	char *res_height_cmt;
+	int hw_tnl;
+	int hw_tnl_key;
+	char *hw_tnl_cmt;
+	int wnd_mode;
+	int wnd_mode_key;
+	char *wnd_mode_cmt;
+	int vsync;
+	int vsync_key;
+	char *vsync_cmt;
+	int tex_qlty;
+	int tex_qlty_key;
+	char *tex_qlty_cmt;
+	int bump_map;
+	int bump_map_key;
+	char *bump_map_cmt;
+	int mirrors;
+	int mirrors_key;
+	char *mirrors_cmt;
+	int track_map;
+	int track_map_key;
+	char *track_map_cmt;
+	int env_map;
+	int env_map_key;
+	char *env_map_cmt;
+	int tex_fil_qlty;
+	int tex_fil_qlty_key;
+	char *tex_fil_qlty_cmt;
+	int an_fil_qlty;
+	int an_fil_qlty_key;
+	char *an_fil_qlty_cmt;
+	int shdws;
+	int shdws_key;
+	char *shdws_cmt;
+	int shdw_type;
+	int shdw_type_key;
+	char *shdw_type_cmt;
+	int ext_steer;
+	int ext_steer_key;
+	char *ext_steer_cmt;
+	int heat_haze;
+	int heat_haze_key;
+	char *heat_haze_cmt;
+} CFG;
+
+void strip_newline(char *str, int len) {
+	int i = 0;
+	for(i=0; i<len; i++) {
+		if(str[i] == '\n') {
+			str[i] = '\0';
+			break;
+		}
+	}
+}
+
+void strip_tab(char *str, int len) {
+	int i = 0;
+	for(i=0; i<len; i++) {
+		if(str[i] == '\t') {
+			str[i] = '\0';
+			break;
+		}
+	}
+}
+
+void save_file(CFG *cfg) {
+	printf("Generating new configuration file from buffer..\n");
+	cfg->fp = fopen("f1graphics.cfg", "w");
+	if(!cfg->fp) {
+		printf("Couldn't write file!\n");
+	} else {
+		for(i=0; i<cfg->tot_lines; i++) {
+			fprintf(cfg->fp, "%s\n", cfg->all_lines[i]);
+		}
+		fclose(cfg->fp);
+		printf("New configuration file created successfully!\n");
+	}
+}
+
+void show_file_contents(CFG *cfg) {
+	char *buff;
+	int counter = 0;
+	cfg->fp = fopen(cfg->file_name, "r");
+	//if(sizeof(buff) > 0) free(buff);
+	buff = (char *) malloc(sizeof(char) * MAX_LINE);
+	while(fgets(buff, MAX_LINE, cfg->fp)) {
+		printf("%d: %s", counter, buff);
+		free(buff);
+		buff = (char *) malloc(sizeof(char) * MAX_LINE);
+		++counter;
+	}
+	free(buff);
+}
+
+void get_val_cmt_(CFG *cfg) {
+	if(sizeof(buff) > 0) free(buff);
+	if(sizeof(value) > 0) free(value);
+	if(sizeof(comment) > 0) free(comment);
+
+	buff = (char *) malloc(sizeof(char) * MAX_LINE);
+	value = (char *) malloc(sizeof(char) * MAX_VAL);
+	comment = (char *) malloc(sizeof(char) * MAX_LINE);
+	
+	strcpy(buff, cfg->all_lines[cfg->cur_sel]);
+	token = strtok(buff, ";");
+
+	if(token != NULL) {
+		//value[0] = '\0';
+		strcpy(value, token);
+		strip_tab(value, MAX_VAL);
+	}
+
+	token = strtok(NULL, ";");
+
+	if(token != NULL) {
+		//comment[0] = '\0';
+		strcpy(comment, token);
+		strip_newline(comment, MAX_LINE);
+	}
+
+}
+
+void set_val_cmt_(CFG *cfg) {
+	int last;
+	free(cfg->all_lines[cfg->cur_sel]);
+	cfg->all_lines[cfg->cur_sel] = (char *) malloc(sizeof(char) * MAX_LINE);
+	cfg->all_lines[cfg->cur_sel][0] = '\0';
+
+	free(value);
+	value = (char *) malloc(sizeof(char) * MAX_VAL);
+	//value[0] = '\0';
+
+	sprintf(value, "%d\t\t\t", val);
+	strcat(cfg->all_lines[cfg->cur_sel], value);
+	strcat(cfg->all_lines[cfg->cur_sel], ";");
+	strcat(cfg->all_lines[cfg->cur_sel], comment);
+
+	last = strlen(cfg->all_lines[cfg->cur_sel]) + 1;
+	cfg->all_lines[cfg->cur_sel][last] = '\0';
+}
+
+void show_cur_val(CFG *cfg) {
+	printf("Change settings as below:\n");
+	printf("%s\n", comment);
+}
+
+void start_(CFG *cfg) {
+	printf("\nType 't' to edit Texture Filter Quality\n");
+	printf("Type 'm' to edit Track Map\n");
+	printf("Type 'v' to edit Environment Map\n");
+	printf("Type 'q' to edit Texture Quality\n");
+	printf("Type 'a' to edit Anisotropic Filter Quality\n");
+	printf("Type 's' to edit Shadow Type\n");
+	printf("Type 'c' to generate configuration file from buffer\n");
+	printf("Type 'x' to exit\n\n");
+
+	scanf("%c", &sel);
+	if(sel == 't' || sel == 'T') {
+		cfg->cur_sel = cfg->tex_fil_qlty_key;
+		get_val_cmt_(cfg);
+		show_cur_val(cfg);
+
+		printf("Enter value for Texture Filter Quality (%s - current): ", value);
+		scanf("%d", &val);
+		cfg->tex_fil_qlty = val;
+
+		strcpy(cfg->tex_fil_qlty_cmt, comment);
+
+		set_val_cmt_(cfg);
+		printf("Value edited successfully..\n");
+		getchar();
+		start_(cfg);
+	} else if(sel == 'm' || sel == 'M') {
+		cfg->cur_sel = cfg->track_map_key;
+		get_val_cmt_(cfg);
+		show_cur_val(cfg);
+
+		printf("Enter value for Track Map (%s - current): ", value);
+		scanf("%d", &val);
+		cfg->track_map = val;
+
+		strcpy(cfg->track_map_cmt, comment);
+
+		set_val_cmt_(cfg);
+		printf("Value edited successfully..\n");
+		getchar();
+		start_(cfg);
+	} else if(sel == 'v' || sel == 'V') {
+		cfg->cur_sel = cfg->env_map_key;
+		get_val_cmt_(cfg);
+		show_cur_val(cfg);
+
+		printf("Enter value for Environment Map (%s - current): ", value);
+		scanf("%d", &val);
+		cfg->env_map = val;
+
+		strcpy(cfg->env_map_cmt, comment);
+
+		set_val_cmt_(cfg);
+		printf("Value edited successfully..\n");
+		getchar();
+		start_(cfg);
+	} else if(sel == 'q' || sel == 'Q') {
+		cfg->cur_sel = cfg->tex_qlty_key;
+		get_val_cmt_(cfg);
+		show_cur_val(cfg);
+
+		printf("Enter value for Texture Quality (%s - current): ", value);
+		scanf("%d", &val);
+		cfg->tex_qlty = val;
+
+		strcpy(cfg->tex_qlty_cmt, comment);
+
+		set_val_cmt_(cfg);
+		printf("Value edited successfully..\n");
+		getchar();
+		start_(cfg);
+	} else if(sel == 'a' || sel == 'A') {
+		cfg->cur_sel = cfg->an_fil_qlty_key;
+		get_val_cmt_(cfg);
+		show_cur_val(cfg);
+
+		printf("Enter value for Anisotropic Filter Quality (%s - current): ", value);
+		scanf("%d", &val);
+		cfg->an_fil_qlty = val;
+
+		strcpy(cfg->an_fil_qlty_cmt, comment);
+
+		set_val_cmt_(cfg);
+		printf("Value edited successfully..\n");
+		getchar();
+		start_(cfg);
+	} else if(sel == 's' || sel == 'S') {
+		cfg->cur_sel = cfg->shdw_type_key;
+		get_val_cmt_(cfg);
+		show_cur_val(cfg);
+
+		printf("Enter value for Shadow Type (%s - current): ", value);
+		scanf("%d", &val);
+		cfg->shdw_type = val;
+
+		set_val_cmt_(cfg);
+		printf("Value edited successfully..\n");
+		getchar();
+		start_(cfg);
+	} else if(sel == 'c' || sel == 'C') {
+		save_file(cfg);
+		getchar();
+		start_(cfg);
+	} else if(sel == 'f' || sel == 'F') {
+		show_file_contents(cfg);
+		getchar();
+		start_(cfg);
+	}
+}
+
+void init_vals(CFG *cfg) {
+	cfg->tex_fil_qlty_key = 98;
+	cfg->tex_fil_qlty_cmt = (char *) malloc(sizeof(char) * MAX_LEN);
+	cfg->tex_fil_qlty = 0;
+	
+	cfg->track_map_key = 54;
+	cfg->track_map_cmt = (char *) malloc(sizeof(char) * MAX_LEN);
+	cfg->track_map = 0;
+
+	cfg->env_map_key = 64;
+	cfg->env_map_cmt = (char *) malloc(sizeof(char) * MAX_LEN);
+	cfg->env_map = 0;
+
+	cfg->tex_qlty_key = 23;
+	cfg->tex_qlty_cmt = (char *) malloc(sizeof(char) * MAX_LEN);
+	cfg->tex_qlty = 0;
+
+	cfg->an_fil_qlty_key = 100;
+	cfg->an_fil_qlty_cmt = (char *) malloc(sizeof(char) * MAX_LEN);
+	cfg->an_fil_qlty = 0;
+
+	cfg->shdw_type_key = 108;
+	cfg->shdw_type_cmt = (char *) malloc(sizeof(char) * MAX_LEN);
+	cfg->shdw_type = 0;
+	
+	printf("Initialized values from file..\n");
+	start_(cfg);
+}
+
+void free_vals(CFG *cfg) {
+	free(cfg->tex_fil_qlty_cmt);
+	free(cfg->track_map_cmt);
+	free(cfg->env_map_cmt);
+	free(cfg->tex_qlty_cmt);
+	free(cfg->an_fil_qlty_cmt);
+	free(cfg->shdw_type_cmt);
+}
+
+void read_file(CFG *cfg) {
+	int num;
+	FILE *handler;
+	printf("Now reading contents of file..\n");
+
+	cfg->tot_lines = 0;
+	//free(cfg->all_lines);
+	//free(cfg->cur_line);
+	/*memset(cfg->all_lines, 0, sizeof(char) * MAX_LINE * MAX_LINES);
+	memset(cfg->cur_line, 0, sizeof(char) * MAX_LINE);*/
+	cfg->all_lines = (char **) malloc(sizeof(char) * MAX_LINE * MAX_LINES);
+	cfg->cur_line = (char *) malloc(sizeof(char) * MAX_LINE);
+
+	while(fgets(cfg->cur_line, MAX_LINE, cfg->fp)) {
+		strip_newline(cfg->cur_line, MAX_LINE);
+		if(strlen(cfg->cur_line) > 0) {
+			cfg->all_lines[cfg->tot_lines] = (char *) malloc(sizeof(char) * MAX_LINE);
+			strcpy(cfg->all_lines[cfg->tot_lines], cfg->cur_line);
+			free(cfg->cur_line);
+			cfg->cur_line = (char *) malloc(sizeof(char *) * MAX_LINE);
+			++cfg->tot_lines;
+		}
+	}
+	fclose(cfg->fp);
+
+	printf("Now creating backup file..\n");
+	handler = fopen("f1graphics.cfg.backup", "w");
+	if(handler) {
+		for(num=0; num<cfg->tot_lines; num++) {
+			fprintf(handler, "%s\n", cfg->all_lines[num]);
+		}
+		fclose(handler);
+		printf("Created backup file successfully..\n");
+	}
+
+	init_vals(cfg);
+}	
+
+/*int main(int argc, char *argv[], char *env[]) {
+	CFG cfg;
+	printf("\n|+++++++++++++++++++++++++++++|");
+	printf("\n| Welcome to GP4 configurator |");
+	printf("\n|+++++++++++++++++++++++++++++|\n");
+
+	printf("Specify the path for the configuration file: ");
+	memset(cfg.file_name, 0, 256);
+	fgets(cfg.file_name, 256, stdin);
+	strip_newline(cfg.file_name, 256);
+
+	if(strlen(cfg.file_name) > 0) {
+		cfg.fp = fopen(cfg.file_name, "r");
+		if(cfg.fp) {
+			printf("Loading from location: %s\n", cfg.file_name);
+			read_file(&cfg);
+		} else {
+			printf("Error reading file!\n");
+		}
+	} else {
+		strcpy(cfg.file_name, "C:\\Program Files\\Infogrames\\Grand Prix 4\\f1graphics.cfg");
+		cfg.fp = fopen(cfg.file_name, "r");
+		if(!cfg.fp) {
+			strcpy(cfg.file_name, "C:\\Program Files (x86)\\Infogrames\\Grand Prix 4\\f1graphics.cfg");
+			cfg.fp = fopen(cfg.file_name, "r");
+			if(!cfg.fp) {
+				printf("Error reading file!\n");
+				return -1;
+			}
+		}
+		printf("Loading from default location: %s\n", cfg.file_name);
+		read_file(&cfg);
+		free_vals(&cfg);
+		free(cfg.cur_line);
+		free(cfg.all_lines);
+		cfg.tot_lines = 0;
+	}
+
+	if(sizeof(buff) > 0) free(buff);
+	if(sizeof(value) > 0) free(value);
+	if(sizeof(comment) > 0) free(comment);
+	
+	return 0;
+}*/
+
+/*void strip_newtab(char *str, int len) {
 	int i = 0;
 	for(i=0; i<len; i++) {
 		if(str[i] == '\t') {
@@ -39,7 +459,7 @@ void strip_newline(char *str, int len) {
 			break;
 		}
 	}
-}
+}*/
 
 void SetWndProperties(WNDCLASSEX *wc, HINSTANCE *hInstance, const char *g_szClassName);
 
@@ -220,7 +640,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 			{
 				hDefault = GetStockObject(DEFAULT_GUI_FONT);
 				g_hbrBackground = (HBRUSH) CreateSolidBrush(RGB(255, 255, 255));
-				strip_newtab("Hello world\n", 20);
+				strip_tab("Hello world\n", 20);
 			}
 			{
 				hLabel = CreateWndLabel(hwnd, "Grand Prix 4 graphics", 10, 10, 200, 20, IDC_LABEL);
@@ -377,10 +797,22 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 								return 0;
 							}
 							if(file_exists(szFileName)) {
-								fp = fopen(szFileName, "r");
-								if(fp) {
-									line = (char *) malloc(STRLEN);
-									lines = (char **) malloc(MAX_LEN);
+								CFG cfg;
+								memset(cfg.file_name, 0, 256);
+								strcpy(cfg.file_name, szFileName);
+								//fp = fopen(szFileName, "r");
+								cfg.fp = fopen(cfg.file_name, "r");
+								if(cfg.fp) {
+									//printf("Loading from location: %s\n", cfg.file_name);
+									read_file(&cfg);
+								} else {
+									//printf("Error reading file!\n");
+									MessageBox(hwnd, "Cannot read file!", szFileName, MB_ICONERROR | MB_OK);
+									return -1;
+								}
+								/*if(fp) {
+									line = (char *) malloc(sizeof(char) * STRLEN);
+									lines = (char **) malloc(sizeof(char) * MAX_LEN);
 									i = 0;
 									j = 0;
 									while(fgets(line, 100, fp)) {
@@ -394,7 +826,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 									fclose(fp);
 								} else {
 									MessageBox(hwnd, "Cannot read file!", szFileName, MB_ICONERROR | MB_OK);
-								}
+								}*/
 							} else {
 								MessageBox(hwnd, "File not found!", szFileName, MB_ICONERROR | MB_OK);
 							}
